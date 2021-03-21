add8de113d401f8f6163f3844cd5d67529b3c6f9
Chun-Hao Lien <vbnm123c+github@gmail.com>
2021-03-21 23:18:46 +0800

    Update twitter-media-downloader.user.js

--- twitter-media-downloader.user.js
+++ twitter-media-downloader.user.js
@@ -1,19 +1,15 @@
 // ==UserScript==
-// @name         Twitter Media Downloader
+// @name         Tweetdeck Media Downloader
 // @namespace    https://github.com/FlandreDaisuki
-// @description  Close service worker at twitter.com and enjoy it.
-// @version      0.2.1
+// @description  Enjoy it.
+// @version      0.3.0
 // @author       FlandreDaisuki
 // @match        https://tweetdeck.twitter.com/
-// @match        https://twitter.com/*
-// @match        https://mobile.twitter.com/*
 // @require      https://unpkg.com/sentinel-js@0.0.5/dist/sentinel.js
-// @resource     dialog.css https://raw.githubusercontent.com/GoogleChrome/dialog-polyfill/master/dist/dialog-polyfill.css
 // @license      MIT
 // @noframes
 // @grant        GM_download
 // @grant        GM_xmlhttpRequest
-// @grant        GM_getResourceText
 // @connect      127.0.0.1
 // ==/UserScript==
 
@@ -23,12 +19,6 @@ const IMG_NAMING_PATTERN = '@{userId}-{twimgId}';
 /* global sentinel */
 
 const SERVER_ORIGIN = `http://127.0.0.1:${PORT}`;
-
-const TWITTER_CSS = `
-.r-11yh6sk .r-184en5c > .r-1ftll1t {
-  z-index: 1;
-  background-color: rgba(0,0,0,0.5);
-}`;
 
 const TWEETDECK_CSS = `
 html .med-tray.js-mediaembed {
@@ -52,7 +42,15 @@ html .med-downloadlink {
 }
 html .med-flaglink {
   grid-area: link-fm;
-}`;
+}
+html dialog {
+  z-index: 301;
+  top: 50%;
+}
+.icon-download:before{
+  content:"\\F186";
+}
+`;
 
 const $ = (s) => document.querySelector(s);
 const $$ = (s) => [...document.querySelectorAll(s)];
@@ -83,15 +81,10 @@ const GM_fetch = (url, details = {}) => {
   });
 };
 
-const isBoundingRectShows = (el) => {
-  const { x } = el.getBoundingClientRect();
-  return x >= 0 && x <= window.innerWidth;
-};
-
 const downloadImage = (imgEl, tweetURL) => {
   const twimgURL = getImageURL(imgEl.src);
   const twimgId = twimgURL.pathname.replace('/media/', '');
-  const [userId, tweetId, imgOrdinal] = [...tweetURL.matchAll(/.*\/(.*)\/status\/(\d+)(?:\/(?:video|photo)\/(\d+))?/)][0].slice(1);
+  const [userId, tweetId, imgOrdinal] = [...tweetURL.matchAll(/.*\/(.*)\/status\/(\d+)(?:\/(?:video|photo)\/(\d+))?/g)][0].slice(1);
   const filename = IMG_NAMING_PATTERN
     .replace('{twimgId}', twimgId)
     .replace('{userId}', userId)
@@ -104,218 +97,82 @@ const downloadImage = (imgEl, tweetURL) => {
 };
 
 // Entry Point
-if (location.host === 'twitter.com' || location.host === 'mobile.twitter.com') {
-  applyStyle(TWITTER_CSS);
-  applyStyle(GM_getResourceText('dialog.css'));
-  twitterImageSetup();
-  twitterVideoSetup();
-}
-else if (location.host === 'tweetdeck.twitter.com') {
+if (location.host === 'tweetdeck.twitter.com') {
   applyStyle(TWEETDECK_CSS);
   tweetdeckSetup();
 }
 
-function twitterImageSetup() {
-  const carouselTrigger = () => {
-    const el = document.createElement('div');
-    el.id = 'carousel-trigger';
-    document.body.appendChild(el);
-    setTimeout(() => {
-      document.body.removeChild(el);
-    }, 1000);
-  };
-
-  const createCarouselPhotoDownloadBtn = () => {
-    const root = document.createElement('div');
-    root.id = 'TMD-carousel-photo-download-btn';
-    root.className = 'css-1dbjc4n r-18u37iz r-16y2uox r-1h0z5md';
-    root.innerHTML = `
-<div role="button" data-focusable="true" tabindex="0" class="css-18t94o4 css-1dbjc4n r-1777fci r-11cpok1 r-1ny4l3l r-bztko3 r-lrvibr">
-  <div dir="ltr" class="css-901oao r-1awozwy r-jwli3a r-6koalj r-1qd0xha r-a023e6 r-16dba41 r-1h0z5md r-ad9z0x r-bcqeeo r-o7ynqc r-clp7b1 r-3s2u2q r-qvutc0">
-    <div class="css-1dbjc4n r-xoduu5 r-1udh08x">
-      <span class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-1n0xq6e r-bcqeeo r-d3hbe1 r-1wgg2b2 r-axxi2z r-qvutc0">
-        <span class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0">下載圖片</span>
-      </span>
-    </div>
-  </div>
-</div>`;
-
-    root.addEventListener('click', (ev) => {
-      ev.stopPropagation();
-      $$('.r-11yh6sk.r-buy8e9 img.css-9pa8cd')
-        .filter(isBoundingRectShows)
-        .map((imgEl) => downloadImage(imgEl, location.href));
-    });
-    return root;
-  };
-
-  const createCarouselVideoDownloadBtn = (isGif) => {
-    const root = document.createElement('div');
-    root.id = 'TMD-carousel-video-download-btn';
-    root.className = 'css-1dbjc4n r-18u37iz r-16y2uox r-1h0z5md';
-    root.innerHTML = `
-<div role="button" data-focusable="true" tabindex="0" class="css-18t94o4 css-1dbjc4n r-1777fci r-11cpok1 r-1ny4l3l r-bztko3 r-lrvibr">
-  <div dir="ltr" class="css-901oao r-1awozwy r-jwli3a r-6koalj r-1qd0xha r-a023e6 r-16dba41 r-1h0z5md r-ad9z0x r-bcqeeo r-o7ynqc r-clp7b1 r-3s2u2q r-qvutc0">
-    <div class="css-1dbjc4n r-xoduu5 r-1udh08x">
-      <span class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-1n0xq6e r-bcqeeo r-d3hbe1 r-1wgg2b2 r-axxi2z r-qvutc0">
-        <span class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0">下載${isGif ? ' GIF 影片' : '影片'}</span>
-      </span>
-    </div>
-  </div>
-</div>`;
-
-    root.addEventListener('click', async() => {
-      const href = location.href.replace(/\/(video|photo)\/.*/, '');
-      const resp = await GM_fetch(`${SERVER_ORIGIN}/_?${href}`).catch(console.error);
-      const result = JSON.parse(resp.responseText);
-      if (result.ok) {
-        console.log('已成功下載');
-      }
-      else {
-        console.error('下載失敗', result.reason);
-      }
-    });
-    return root;
-  };
-
-  sentinel.on('.r-g6jmlv', (carouselRoot) => {
-    console.info('大圖展示開啟！', carouselRoot);
-  });
-
-  sentinel.on('.r-g6jmlv [role="group"]', (groupRoot) => {
-    console.info('功能按鈕！', groupRoot);
-    carouselTrigger();
-  });
-
-  sentinel.on('.r-g6jmlv .r-o7ynqc [role="button"]', (pagingBtn) => {
-    console.info('換頁按鈕！', pagingBtn);
-    pagingBtn.addEventListener('click', carouselTrigger);
-  });
-
-  sentinel.on('div#carousel-trigger', (trigger) => {
-    console.info('大圖觸發！', trigger);
-    console.log(location.href);
-
-    setTimeout(() => {
-      const currentListItem = $$('[role="listitem"]').filter(isBoundingRectShows)[0];
-      const isVideo = currentListItem && currentListItem.querySelector('video');
-      const isGIF = isVideo && currentListItem.querySelector('video[src^="https"]');
-
-      const groupRoot = $('.r-g6jmlv [role="group"]');
-      [...groupRoot.querySelectorAll('[id^="TMD-carousel-"]')].map((btn) => btn.remove());
-      if (isVideo) {
-        groupRoot.insertAdjacentElement('afterbegin', createCarouselVideoDownloadBtn(isGIF));
-      }
-      else {
-        groupRoot.insertAdjacentElement('afterbegin', createCarouselPhotoDownloadBtn());
-      }
-    }, 200);
-  });
-
-  applyStyle();
-}
-
-function twitterVideoSetup() {
+function tweetdeckSetup() {
   const dialog = document.createElement('dialog');
   dialog.className = 'fixed';
   document.body.appendChild(dialog);
 
-  const bindClickThenReturn = (btn, href) => {
-    btn.addEventListener('click', async() => {
-      const resp = await GM_fetch(`${SERVER_ORIGIN}/_?${href}`).catch(console.error);
-      const result = JSON.parse(resp.responseText);
-      if (result.ok) {
-        dialog.textContent = '已成功下載 ' + result.dest;
-        console.log('已成功下載', result.dest);
-        dialog.setAttribute('open', 'open');
-        setTimeout(() => {
-          dialog.removeAttribute('open');
-        }, 3000);
-      }
-      else {
-        dialog.textContent = '下載失敗';
-        console.error('下載失敗', result.reason);
-      }
-    });
-
-    return btn;
-  };
-
-  const createTimelineVideoDownloadBtn = (articleEl, isGif) => {
-    const root = document.createElement('div');
-    root.id = 'TMD-timeline-video-download-btn';
-    root.className = 'css-1dbjc4n r-18u37iz r-16y2uox r-1h0z5md';
-    root.innerHTML = `
-<div role="button" data-focusable="true" tabindex="0" class="css-18t94o4 css-1dbjc4n r-1777fci r-11cpok1 r-1ny4l3l r-bztko3 r-lrvibr">
-  <div dir="ltr" class="css-901oao r-1awozwy r-111h2gw r-6koalj r-1qd0xha r-a023e6 r-16dba41 r-1h0z5md r-ad9z0x r-bcqeeo r-o7ynqc r-clp7b1 r-3s2u2q r-qvutc0">
-    <div class="css-1dbjc4n r-xoduu5 r-1udh08x">
-      <span class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-1n0xq6e r-bcqeeo r-d3hbe1 r-1wgg2b2 r-axxi2z r-qvutc0">
-        <span class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0">下載${isGif ? ' GIF 影片' : '影片'}</span>
-      </span>
-    </div>
-  </div>
-</div>`;
-    return bindClickThenReturn(root, articleEl.querySelector('a[title][aria-label]').href);
-  };
-
-  const createTweetVideoDownloadBtn = (isGif) => {
-    const root = document.createElement('div');
-    root.id = 'TMD-tweet-video-download-btn';
-    root.className = 'css-1dbjc4n r-18u37iz r-1h0z5md r-3qxfft r-h4g966 r-rjfia';
-    root.innerHTML = `
-<div role="button" data-focusable="true" tabindex="0" class="css-18t94o4 css-1dbjc4n r-1777fci r-11cpok1 r-1ny4l3l r-bztko3 r-lrvibr">
-  <div dir="ltr" class="css-901oao r-1awozwy r-111h2gw r-6koalj r-1qd0xha r-a023e6 r-16dba41 r-1h0z5md r-ad9z0x r-bcqeeo r-o7ynqc r-clp7b1 r-3s2u2q r-qvutc0">
-    <div class="css-1dbjc4n r-xoduu5">
-      <div class="css-1dbjc4n r-sdzlij r-1p0dtai r-xoduu5 r-1d2f490 r-xf4iuw r-u8s1d r-zchlnj r-ipm5af r-o7ynqc r-6416eg">下載${isGif ? ' GIF 影片' : '影片'}</div>
-    </div>
-  </div>
-</div>`;
-    return bindClickThenReturn(root, location.href);
-  };
-
-  const cache = new WeakSet();
-
-  sentinel.on('article[role="article"] video', function(video) {
-    if (cache.has(video)) { return; }
-
-    const isGif = video.src.startsWith('https');
-    if (isGif) {
-      console.info('新GIF！', video);
+  const createDownloadHandler = (href) => async function clickToDownloadVideo() {
+    const resp = await GM_fetch(`${SERVER_ORIGIN}/?${href}`).catch(console.error);
+    const result = JSON.parse(resp.responseText);
+    if (result.ok) {
+      dialog.textContent = '已成功下載 ' + result.dest;
+      console.log('已成功下載', result.dest);
+      dialog.setAttribute('open', 'open');
+      setTimeout(function dialogAutoClose() {
+        dialog.removeAttribute('open');
+      }, 3000);
     }
     else {
-      console.info('新影片！', video);
+      dialog.textContent = '下載失敗';
+      console.error('下載失敗', result.reason);
     }
+  };
 
-    const articleEl = video.closest('article');
+  sentinel.on('.med-origlink', (origlinkEl) => {
+    const a = document.createElement('a');
+    a.className = 'med-downloadlink';
+    a.textContent = 'Download';
+    a.href = 'javascript:;';
 
-    const timelineGroupEl = articleEl.querySelector('.r-1mdbhws[role="group"]');
-    if (timelineGroupEl) {
-      const btn = createTimelineVideoDownloadBtn(articleEl, isGif);
-      timelineGroupEl.insertAdjacentElement('afterbegin', btn);
-    }
-
-    const tweetGroupEl = articleEl.querySelector('.r-a2tzq0[role="group"]');
-    if (tweetGroupEl) {
-      const btn = createTweetVideoDownloadBtn(isGif);
-      tweetGroupEl.insertAdjacentElement('afterbegin', btn);
-    }
-  });
-}
-
-function tweetdeckSetup() {
-  sentinel.on('.med-origlink', (origLinkEl) => {
     const mediaImg = $('.media-img');
     if (mediaImg) {
-      const a = document.createElement('a');
       a.id = 'TMD-photo-download-btn';
-      a.className = 'med-downloadlink';
-      a.textContent = 'Download';
-      a.href = 'javascript:;';
-      a.onclick = () => {
+      a.onclick = function clickToDownloadImage() {
         downloadImage(mediaImg, $('#open-modal time > a').href);
       };
 
-      origLinkEl.insertAdjacentElement('afterend', a);
+      origlinkEl.insertAdjacentElement('afterend', a);
     }
+
+    const mediaVideo = $('.js-media-native-video');
+    if (mediaVideo) {
+      a.id = 'TMD-video-download-btn';
+      a.onclick = createDownloadHandler($('.tweet-timestamp a').href);
+      origlinkEl.insertAdjacentElement('afterend', a);
+    }
+  });
+
+  sentinel.on('.js-media-preview-container.is-gif, .js-media-preview-container.is-video', (previewContainer) => {
+    const article = previewContainer.closest('article.stream-item');
+    if (!article) { return; }
+
+    const tweetActions = article.querySelector('.js-tweet-actions');
+    if (!tweetActions) { return; }
+    if (tweetActions.querySelector('a[rel="download"]')) { return; }
+
+    // <li class="tweet-action-item pull-left margin-r--10">
+    //   <a class="tweet-action position-rel" href="#" rel="download">
+    //     <i class="icon icon-download txt-center pull-left"></i>
+    //     <span class="is-vishidden">Download</span>
+    //   </a>
+    // </li>
+    const actionItem = document.createElement('li');
+    actionItem.className = 'tweet-action-item pull-left margin-r--10';
+
+    const a = document.createElement('a');
+    a.innerHTML = `<i class="icon icon-download txt-center pull-left"></i> <span class="is-vishidden">Download</span> <span class="pull-right margin-l--2 margin-t--1 txt-size--12">下載</span>`;
+    a.className = 'tweet-action position-rel';
+    a.href = 'javascript:;'
+    a.rel = 'download';
+    a.onclick = createDownloadHandler(article.querySelector('.tweet-timestamp a').href);
+
+    actionItem.appendChild(a);
+    tweetActions.insertAdjacentElement('afterbegin', actionItem);
   });
 }
